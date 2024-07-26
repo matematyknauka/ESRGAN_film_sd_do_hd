3ea0a7ed18f1d5e9155c8e9821848d45efd1e3ed
matematyknauka <matematyknauka@gmail.com>
2024-07-26 19:58:05 +0000

    Initial commit

--- test.py
+++ test.py
@@ -1,37 +1,57 @@
-import os.path as osp
-import glob
+import sys
 import cv2
-import numpy as np
 import torch
+import numpy as np
+from moviepy.editor import VideoFileClip
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+# Dodaj ścieżkę do repozytorium ESRGAN
+# sys.path.append('/ESRGAN_sd_do_hd') nie wiem czy zadziała
+
+# Importuj definicję modelu
 import RRDBNet_arch as arch
 
-model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
-# device = torch.device('cpu')
+class ESRGAN:
+    def __init__(self, model_path):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = self.load_model(model_path)
+        
+    def load_model(self, model_path):
+        model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+        model.load_state_dict(torch.load(model_path), strict=True)
+        model.eval()
+        model.to(self.device)
+        return model
+    
+    def enhance_frame(self, frame):
+        img = frame.astype(np.float32) / 255.0  # Normalizacja
+        img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+        img_LR = img.unsqueeze(0).to(self.device)
 
-test_img_folder = 'LR/*'
+        with torch.no_grad():
+            output = self.model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+        output = (output * 255.0).round().astype(np.uint8)
+        return output
 
-model = arch.RRDBNet(3, 3, 64, 23, gc=32)
-model.load_state_dict(torch.load(model_path), strict=True)
-model.eval()
-model = model.to(device)
+def enhance_video(input_file, output_file, model_path):
+    video = VideoFileClip(input_file)
+    esrgan = ESRGAN(model_path)
+    
+    def process_frame(frame):
+        frame = np.array(frame)  # Konwertuj do numpy array
+        enhanced_frame = esrgan.enhance_frame(frame)  # Popraw jakość
+        return enhanced_frame  # Zwróć numpy array
 
-print('Model path {:s}. \nTesting...'.format(model_path))
+    # Przetwarzaj wideo
+    video_hd = video.fl_image(process_frame)
+    video_hd.write_videofile(output_file, codec='libx264')
 
-idx = 0
-for path in glob.glob(test_img_folder):
-    idx += 1
-    base = osp.splitext(osp.basename(path))[0]
-    print(idx, base)
-    # read images
-    img = cv2.imread(path, cv2.IMREAD_COLOR)
-    img = img * 1.0 / 255
-    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
-    img_LR = img.unsqueeze(0)
-    img_LR = img_LR.to(device)
+# Ścieżki do plików
+input_file = "LR/Kamil_sd.mp4"  # Ścieżka do sd
+output_file = "results/Kamil_hd.mp4"  # Ścieżka do hd
+model_path = "models/RRDB_ESRGAN_x4.pth"  # Ścieżka do modelu ESRGAN
 
-    with torch.no_grad():
-        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
-    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-    output = (output * 255.0).round()
-    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+# Uruchom przetwarzanie wideo
+enhance_video(input_file, output_file, model_path)
